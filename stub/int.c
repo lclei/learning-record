caf56ec6edc87e6991c3a67f40a32b2de69b217d
lclei <lclei@mail.ustc.edu.cn>
2019-04-25 13:45:37 +0000

    add stub

---
+++ stub/int.c
@@ -0,0 +1,11 @@
+/* example stub */
+/* int.c */
+/* lcl 20190424 */
+#include <stdio.h>
+#include <malloc.h>
+
+int main(){
+	    int *p = malloc(32);
+		    free(p);
+			    return 0;
+}
---
+++ stub/malloc.h
@@ -0,0 +1,5 @@
+/* malloc.h on local */
+#define malloc(size) mymalloc(size)
+#define free(ptr) myfree(ptr)
+void *mymalloc(size_t size);
+void myfree(void* ptr);
